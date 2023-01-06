2eb575e693167f73c85718c1325f7161ddc577e7
G Ganesh <121279015+ga7nesh@users.noreply.github.com>
2023-01-06 21:06:59 +0530

    Time: 1035 ms (5.03%), Space: 79.5 MB (5.07%) - LeetHub

---
+++ 0006-zigzag-conversion/0006-zigzag-conversion.js
@@ -0,0 +1,38 @@
+let convert = function(s, numRows) {
+    if (numRows == 1) {
+        return s;
+    }
+
+    let n = s.length;
+    let sections = Math.ceil(n / (2 * numRows - 2.0));
+    let numCols = sections * (numRows - 1);
+
+    let matrix = new Array(numRows).fill(0).map(() => new Array(numCols).fill(' '));
+    
+    let currRow = 0, currCol = 0;
+    let currStringIndex = 0;
+
+    // Iterate in zig-zag pattern on matrix and fill it with string characters.
+    while (currStringIndex < n) {
+        // Move down.
+        while (currRow < numRows && currStringIndex < n) {
+            matrix[currRow][currCol] = s[currStringIndex];
+            currRow++;
+            currStringIndex++;
+        }
+
+        currRow -= 2;
+        currCol++;
+
+        // Move up (with moving right also).
+        while (currRow > 0 && currCol < numCols && currStringIndex < n) {
+            matrix[currRow][currCol] = s[currStringIndex];
+            currRow--;
+            currCol++;
+            currStringIndex++;
+        }
+    }
+
+    let answer = matrix.map(row => row.join('')).join('');
+    return answer.replaceAll(' ', '');
+};
