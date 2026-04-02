1ad666aa9b3e5e4069b3201a02962e22e44c4e72
Ganesh <gundavarapuganesh@gmail.com>
2026-04-02 22:53:20 +0530

    Time: 55 ms (5.04%) | Memory: 42.5 MB (70.30%) - LeetSync

--- 69-sqrtx/sqrtx.java
+++ 69-sqrtx/sqrtx.java
@@ -1,10 +1,14 @@
 class Solution {
     public int mySqrt(int x) {
         int res = 0;
-         if(x==0 || x==1) return x;
-        for(int i=1;i<=x/i;i++){
+
+        if (x == 0 || x == 1)
+            return x;
+
+        for (int i = 1; i <= x / i; i++) {
             res = i;
         }
+
         return res;
     }
 }
